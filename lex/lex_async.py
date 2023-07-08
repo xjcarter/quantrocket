a4fe05665da449f4e04b1014b51f02fdb1aa57b0
James Carter <xjcarter@gmail.com>
2023-07-08 01:16:30 -0400

    working on lex_async

--- lex/lex_async.py
+++ lex/lex_async.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from quantrocket.blotter import order_statuses, OrderStatuses
 from quantrocket.realtime import collect_market_data
 import asyncio
-from posmgr import PosMgr
+from posmgr import PosMgr, TradeSide
+import calendar_calcs
+import os
+
 
 # Connect to QuantRocket
 # Make sure you have a running QuantRocket deployment
@@ -11,6 +14,8 @@ from posmgr import PosMgr
 
 # Set the account
 account = "YOUR_ACCOUNT_NAME"
+
+YAHOO_DATA_DIRECTORY = os.environment.get('YAHOO_DATA_DIRECTORY', '/home/jcarter/work/trading/data/')
 
 
 START_TIME = "09:25"
@@ -22,13 +27,46 @@ def time_until(date_string):
     now = datetime.now()
     return schedutils.seconds_until(now, date_string) 
 
-def load_historical_data():
+def load_historical_data(symbol):
     ## load yahoo OHLC data
+    try:
+        stock_file = f'{YAHOO_DATA_DIRECTORY}/{symbol}.csv'
+        stock_df = pandas.read_csv(stock_file)
+        stock_df.set_index('Date', inplace=True)
+    except Exception as e:
+        raise e 
 
-def place_entry(data):
+def place_entry(symbol, data):
     ## load in calendar
-    ## load in MondayAnchor 
     ## return True False to trade
+    daysback = 20
+    holidays = calendar_calcs.load_holidays()
+    anchor = MondayAnchor(derived_len=days_back)
+
+    ss = len(data)
+    if ss < daysback:
+        raise RuntimeError(f'Not enoungh data points for {symbol}: len={ss}, daysback={daysback}')
+        continue
+
+    gg = stock_df[-days_back:]
+
+    for i in range(gg.shape[0]):
+        idate = gg.index[i]
+        stock_bar = gg.loc[idate]
+        cur_dt = datetime.strptime(idate,"%Y-%m-%d").date()
+
+        tt = anchor.push((cur_dt, stock_bar))
+        if tt is not None:
+            anchor_bar, mp = tt
+            anchor_low = anchor_bar['Low']
+
+    ## FIX This 
+    cc = corr.valueAt(0)
+    bb = beta.valueAt(0)
+
+   
+
+
 
 def place_exit(current_price, entry_price, duration):
     ## load in calendar
@@ -57,8 +95,16 @@ def create_order(side, amount, symbol):
     return ticket 
 
 
-def get_current_price():
+def get_current_price(symbol):
     ## get current realtime price
+
+def calc_trade_amount(symbol, trade_capital, fudge_factor=1):
+    ## FIX THIS 
+    ## get current price 
+    ## problem: how to do handle price moving away from current price before fill?
+    ## currently using fudge factor <= 1 to handle problem
+    current_price = get_current_price(symbol)
+    return int( (trade_capital * fudge_factor)/current_price )
 
 async def handle_trade_fills():
     ## handle fills and post all files through PosMgr object
@@ -100,22 +146,40 @@ async def handle_trade_fills():
 
 async def main(strategy_id, universe):
 
-    load_current_positions(stratefgy_id, universe) 
+    pmgr = PosMgr(strategy_id=strategy_id, universe=universe)
+    pmgr.load_all()
 
-    data = load_historical_data()
+    pp = pmgr.position_count()
+    if pp == 0:
+        raise RuntimeError(f'No targeted positions for universe: {universe}')
+    if pp != 1:
+        raise RuntimeError(f'Too many names: {pmgr.positions} - this a single name strategy')
+
+    ## grab the only instrument in the universe
+    symbol = universe[0]
+
+    ## returns a PosNode object
+    position_node = pmgr.get_position(symbol)
+    current_pos = position_node.position
+
+    data = load_historical_data(symbol)
     
     start_time, secs_until_start = time_until(START_TIME)
     await asyncio.sleep(secs_until_start)
 
-    if current_pos == 0 and place_entry(data):
-        asyncio.create_task(handle_trade_fills)
-        open_time, secs_until_open = time_until(OPEN_TIME)
-        await asyncio.sleep(secs_until_open)
-        entry_tkt = create_order(TradeSide.BUY, symbol, trade_amt)
+    if current_pos == 0 and place_entry(symbol, data):
+        trade_amt = calc_trade_amount(symbol, position_node.trade_capital)
+        if trade_amt > 0:
+            asyncio.create_task(handle_trade_fills)
+            open_time, secs_until_open = time_until(OPEN_TIME)
+            await asyncio.sleep(secs_until_open)
+            entry_tkt = create_order(TradeSide.BUY, symbol, trade_amt)
 
     exit_time, secs_until_exit = time_until(EXIT_TIME)
     await asyncio.sleep(secs_until_exit)
 
+    position_node = pmgr.get_position(symbol)
+    current_pos, duration = position_node.position, position_node.duration
     current_price = get_current_price()
     if current_pos > 0 and place_exit(current_price, entry_price, duration)
         asyncio.create_task(handle_trade_fills)
@@ -126,6 +190,6 @@ async def main(strategy_id, universe):
 
 
 if __name__ == "__main__":
-    asyncio.run(main(u.strategy_id, universe=universe))
+    asyncio.run( main(u.strategy_id, universe=universe) )
 
     
