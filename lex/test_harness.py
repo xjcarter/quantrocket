ac024f28f93d3964920c79a6e9d4fe5e3bc91aee
James Carter <xjcarter@gmail.com>
2023-07-12 19:18:09 -0400

    update harness

--- lex/test_harness.py
+++ lex/test_harness.py
@@ -1,7 +1,49 @@
 import pandas
 import uuid
+from random import randint
 
 order_queue = list()
+bids = [ -0.03, -0.03, -0.02, -0.02, 0, 0, 0.02, 0.04, -0.02, 0,02, 0.03, 0, 0, 0.03, 0.01, 0.02, 0.03 ]
+asks = [ x + 0.01 for x in bids ]
+ref_close = None
+
+YAHOO_DATA_DIRECTORY = os.environment.get('YAHOO_DATA_DIRECTORY', '/home/jcarter/work/trading/data/')
+
+## force yesterday's close to be above/below the current anchor
+## use adjust_close to tweak the close relative to the current anchor
+def alter_data_to_anchor(stock_df, adjust_close=0):
+    global ref_close 
+
+    from indicators import MondayAnchor
+    
+    anchor = MondayAnchor(50)
+    gg = stock_df[60:]
+
+    last_index = None
+    for i in range(gg.shape[0]):
+        idate = gg.index[i]
+        stock_bar = gg.loc[idate]
+        cur_dt = datetime.strptime(idate,"%Y-%m-%d").date()
+        anchor.push((cur_dt, stock_bar))
+        last_index = idate
+
+    anchor_bar = anchor.valueAt(1)
+    ref_close = anchor_bar['Close'] + adjust_close
+    stock_df.at[last_index,'Close'] = ref_close
+
+    return stock_df 
+
+
+def fill_price():
+    global bids 
+    global asks
+    global ref_close
+
+    i = randint(0,len(bids))
+    bid, ask = bids[i], asks[i]
+    ref_close = 0.5 * (2*ref_close +bid + ask )
+    return ref_close
+
 
 #order_id = place_order(account, symbol, quantity, action, order_type)
 def place_order(account, symbol, quantity, action, order_type):
@@ -11,6 +53,7 @@ def place_order(account, symbol, quantity, action, order_type):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     unique_id = str(uuid.uuid4())
 
+    order = dict()
     order = {
         "account": account,
         "symbol": symbol,
@@ -19,6 +62,7 @@ def place_order(account, symbol, quantity, action, order_type):
         "order_type": order_type
         "OrderRef": unique_id
         "timestamp": timestamp
+        "price": fill_price() 
     }
 
     order_queue.append(order)
@@ -40,13 +84,14 @@ bid_price = prices.loc[symbol, "Bid"]
 ask_price = prices.loc[symbol, "Ask"]
 """
 def get_prices(symbol_list, fields):
-    from random import randint
-    c = []
-    for symbol in symbol_list:
-        bid = randint(0,100)
-        ask = randint(0,10)+bid
-        c.append([symbol,bid,ask])
-    df = pandas.DataFrame(columns=['symbol','Bid','Ask'],data=c)
+    global bids
+    global asks
+    global ref_close 
+
+    symbol = symbol_list[0]
+    i = randint(0,len(bids))
+    bid, ask = ref_close + bids[i], ref_close + asks[i]
+    df = pandas.DataFrame(columns=['symbol','Bid','Ask'],data=[symbol, bid, ask])
     df.set_index('symbol', inplace=True)
     return df
 
