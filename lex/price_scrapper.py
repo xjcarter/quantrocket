93921357c5c475619f97762eb86990e3a2043881
James Carter <xjcarter@gmail.com>
2023-07-30 22:53:47 -0400

    add price snap

---
+++ lex/price_scrapper.py
@@ -0,0 +1,108 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_bid_ask(symbol):
+
+    def _strip(quote_str):
+        ## expecting a string in the form: "price x size", ie.  51.20 x 1000 
+        price, volume = [float(x.strip()) for x in quote_str.split('x')]
+        return price, volume
+
+    try:
+        url = f'https://finance.yahoo.com/quote/{symbol}'
+
+        ## tells the site what browser you are using
+        ## to find out this info - just go to google.com and type: 'my user agent'
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        }
+
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        ## return price x size, ie.  51.20 x 1000 
+        bid_quote = soup.find('td', {'data-test': 'BID-value'}).text
+        ask_quote = soup.find('td', {'data-test': 'ASK-value'}).text
+
+        if all([bid_quote, ask_quote]):
+            bid_price, bid_volume = _strip(bid_quote)
+            ask_price, ask_volume = _strip(ask_quote)
+        else:
+            bid_price = ask_price = None
+
+        return bid_price, ask_price
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+        print("Error: Unable to retrieve data. Please check your internet connection.")
+        return None, None 
+    except Exception as e:
+        print(f"Error: {e}")
+        return None, None 
+
+
+class PriceSnapper:
+    def __init__(self, symbol, history_limit=3000):
+        self.prices = list()
+        self.symbol = symbol
+        self.history_limit = history_limit
+
+    def snap_prices(self):
+
+        def _strip(quote_str):
+            ## expecting a string in the form: "price x size", ie.  51.20 x 1000 
+            price, volume = [float(x.strip()) for x in quote_str.split('x')]
+            return price, volume
+
+        try:
+            url = f'https://finance.yahoo.com/quote/{self.symbol}'
+
+            ## tells the site what browser you are using
+            ## to find out this info - just go to google.com and type: 'my user agent'
+            headers = {
+                'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+            }
+
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            ## return price x size, ie.  51.20 x 1000 
+            bid_quote = soup.find('td', {'data-test': 'BID-value'}).text
+            ask_quote = soup.find('td', {'data-test': 'ASK-value'}).text
+
+            if all([bid_quote, ask_quote]):
+                bid_price, bid_volume = _strip(bid_quote)
+                ask_price, ask_volume = _strip(ask_quote)
+            else:
+                bid_price = ask_price = None
+
+            mid_price = None
+            if all([bid_price, ask_price]):
+                mid_price= round(0.5*(bid_price+ask_price),2)
+                self.prices.append(mid_price)
+                if len(self.prices) > self.history_limit:
+                    self.prices = self.prices[-self.history_limit:]
+            return bid_price, ask_price, mid_price
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None, None, None 
+        except Exception as e:
+            print(f"Error: {e}")
+            return None, None, None 
+
+
+if __name__ == "__main__":
+    bid_price, ask_price  = get_bid_ask('SPY')
+
+    print(f"Bid Price: {bid_price}")
+    print(f"Ask Price: {ask_price}")
+
+    p = PriceSnapper('SPY')
+    print(p.snap_prices())
+
+
---
+++ lex/test_price_snap.py
@@ -0,0 +1,36 @@
+
+from price_scrapper import PriceSnapper
+from clockutils import TripWire, time_from_str
+import time
+from datetime import datetime
+import pandas
+
+symbol = 'UPRO'
+p = PriceSnapper(symbol)
+constant_snap = TripWire(time_from_str("22:46"), interval_reset=60, stop_at=time_from_str("22:50"))
+eod = TripWire(time_from_str("22:50"))
+
+time_series = []
+while True:
+    with constant_snap as snap_prices:
+        if snap_prices:
+            bid, ask, mid = p.snap_prices()
+            print(f'snap: {bid}, {ask}')
+            nn = datetime.now()
+            dt_snap = nn.strftime("%Y%m%d")
+            time_snap = nn.strftime("%H%M%S")
+            time_series.append([dt_snap, time_snap, symbol, mid])
+
+    with eod as end_of_day:
+        if end_of_day:
+            print('end of day')
+            break
+
+    time.sleep(1)
+
+df = pandas.DataFrame(columns=['Date','Time', 'Symbol', 'Price'], data=time_series)
+df.set_index('Date', inplace=True)
+today = datetime.now().strftime("%Y%m%d")
+df.to_csv(f'{symbol}_snaps_{today}.py')
+            
+    
